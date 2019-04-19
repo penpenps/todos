b1a73ce252269172c5d155d5f711082543e875fd
Peng, Shu(speng) <speng@paypal.com>
2019-04-19 10:43:58 +0800

    init commit

---
+++ src/client/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Loading.. please wait!</h1>
+      </div>
+    );
+  }
+}
